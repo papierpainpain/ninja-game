2c4add0b6eb2eccf7ac178de670dc16e10fed36b
PapierPain <gourves.seven@gmail.com>
2024-05-26 14:36:59 +0200

    :sparkles: feat: Optimize the game tiles rendering

--- libs/tilemap.py
+++ libs/tilemap.py
@@ -43,7 +43,10 @@ class Tilemap:
         for tile in self.tiles_around(position):
             if tile["type"] in PHYSICS_TILES:
                 rects.append(pygame.Rect(
-                    tile["pos"][0] * self.tile_size, tile["pos"][1] * self.tile_size, self.tile_size, self.tile_size))
+                    tile["pos"][0] * self.tile_size, tile["pos"][1] *
+                    self.tile_size,
+                    self.tile_size, self.tile_size
+                ))
 
         return rects
 
@@ -52,7 +55,13 @@ class Tilemap:
             surface.blit(
                 self.game.assets[tile["type"]][tile["variant"]], (tile["pos"][0] - offset[0], tile["pos"][1] - offset[1]))
 
-        for location in self.tilemap:
-            tile = self.tilemap[location]
-            surface.blit(
-                self.game.assets[tile["type"]][tile["variant"]], (tile["pos"][0] * self.tile_size - offset[0], tile["pos"][1] * self.tile_size - offset[1]))
+        for x in range(offset[0] // self.tile_size, (offset[0] + surface.get_width()) // self.tile_size + 1):
+            for y in range(offset[1] // self.tile_size, (offset[1] + surface.get_height()) // self.tile_size + 1):
+                location = f"{x};{y}"
+                if location in self.tilemap:
+                    tile = self.tilemap[location]
+                    surface.blit(
+                        self.game.assets[tile["type"]][tile["variant"]],
+                        (tile["pos"][0] * self.tile_size - offset[0],
+                         tile["pos"][1] * self.tile_size - offset[1])
+                    )
